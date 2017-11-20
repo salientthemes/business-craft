a82d058c0f08dc551bb40c8d5d10c7626369983e
unknown <cloudsudip@gmail.com>
2017-11-20 12:22:24 +0545

    added new feature

--- front-page.php
+++ front-page.php
@@ -24,13 +24,6 @@ if ( 'posts' == get_option( 'show_on_front' ) ) {
         $business_craft_static_page = 1;
         if ($business_craft_static_page == 1) { ?>
             <div id="content" class="site-content container">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <header class="entry-headers">
-                        <?php the_title( '<h1 class="entry-titles">', '</h1>' ); ?>
-                    </header><!-- .entry-header -->
-                </div>
-            </div>
                 <div id="primary" class="content-area col-sm-8">
                     <main id="main" class="site-main" role="main">
 
