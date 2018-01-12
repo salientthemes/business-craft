5eba90d2a9a1a0058261cd781792b4a49db480b1
kabiraj lamichhane <cloudkabiraj@gmial.com>
2018-01-12 17:58:33 +0545

    fixing error

--- inc/customizer/color/color.php
+++ inc/customizer/color/color.php
@@ -9,6 +9,7 @@ global $business_craft_customizer_defaults;
 
 $business_craft_customizer_defaults['business-craft-primary-color'] = '#37ce9c';
 $business_craft_customizer_defaults['business-craft-section-header-color'] = '#000';
+$business_craft_customizer_defaults['business-craft-color-reset'] = '';
 
 // create a section for color option
 $business_craft_sections['business_crfat_color_section'] 	=
@@ -16,6 +17,43 @@ $business_craft_sections['business_crfat_color_section'] 	=
 		'title'			=>esc_html__('Colors','business-craft'),
 		'priority'		=> 40
 	);
+
+
+/**
+ * Reset color settings to default
+ * @param  $input
+ *
+ * @since business-craft- 1.0
+ */
+if ( ! function_exists( 'business_craft_color_reset' ) ) :
+    function business_craft_color_reset( ) {
+        
+            global $business_craft_customizer_saved_values;
+           $business_craft_customizer_saved_values = business_craft_get_all_options();
+        if ( $business_craft_customizer_saved_values['business-craft-color-reset'] == 1 ) {
+            global $business_craft_customizer_defaults;
+            global $business_craft_customizer_saved_values;
+
+            /*setting fields */
+            $business_craft_customizer_saved_values['business-craft-primary-color'] = $business_craft_customizer_defaults['business-craft-primary-color'];
+            $business_craft_customizer_saved_values['business-craft-section-header-color'] = $business_craft_customizer_defaults['business-craft-section-header-color'];
+            
+
+
+
+            remove_theme_mod( 'background_color' );
+            $business_craft_customizer_saved_values['business-craft-color-reset'] = '';
+            /*resetting fields*/
+            business_craft_reset_options( $business_craft_customizer_saved_values );
+
+        }
+        else 
+        {
+            return '';
+        }
+    }
+endif;
+add_action( 'customize_save_after','business_craft_color_reset' );
 
 // control section for primary color
 $business_craft_settings_controls['business-craft-primary-color']  =  
@@ -45,6 +83,22 @@ $business_craft_settings_controls['business-craft-section-header-color']  =
 			'section'			=>'business_crfat_color_section',
 			'type'				=>'color',
 			'priority'			=>20,
+			'active_callback'	=>'',
+		)
+	);
+
+// control section for reset color
+$business_craft_settings_controls['business-craft-color-reset']  =  
+	array(
+		'setting'			=>array(
+			'default'		=> $business_craft_customizer_defaults['business-craft-color-reset']
+		),
+		'control'				=>array(
+			'label'				=>esc_html__('Rest','business-craft'),
+			'description'		=>esc_html__('Caution: Reset all above color settings to default. Refresh the page after save to view the effects.','business-craft'),
+			'section'			=>'business_crfat_color_section',
+			'type'				=>'checkbox',
+			'priority'			=>30,
 			'active_callback'	=>'',
 		)
 	);
