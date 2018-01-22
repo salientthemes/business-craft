7772ed5b9b3278b3a9182f2e1db847ae884c56d4
pradip <cloudpradip@gmail.com>
2018-01-22 17:17:04 +0545

    responsive fixing

--- inc/hooks/home-our-service.php
+++ inc/hooks/home-our-service.php
@@ -166,7 +166,7 @@ if ( ! function_exists( 'business_craft_home_service_section' ) ) :
                         }
                         ?>
                         <div class="col-md-12 imgchangeabale-div-wrap">
-                            <div class="col-md-6 col-xs-12 col-sm-6">
+                            <div class="col-md-6 col-xs-12 col-sm-12">
                                 <div class="icon-left">
                                     <div class="col-md-12 col-sm-12 col-xs-12">
                                         <a href="#tab-1" data-toggle="tab">
@@ -203,7 +203,7 @@ if ( ! function_exists( 'business_craft_home_service_section' ) ) :
                                     </div><!-- col-md-4 -->                             
                                 </div>
                             </div>
-                            <div class="col-md-6 col-xs-hidden col-sm-6">
+                            <div class="col-md-6 col-sm-hidden">
                                 <div class="tab-content">
                                     <div class="tab-pane active" id="tab-1" style = "background : url('<?php echo esc_url($about_image_1);?>')"></div>
                                     <div class="tab-pane" id="tab-2" style = "background : url('<?php echo esc_url($about_image_2 );?>')"></div>
