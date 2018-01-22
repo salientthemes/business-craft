a8cf8b7a2473a862f5b4be3c8c81d5715a4b682a
kabiraj lamichhane <cloudkabiraj@gmial.com>
2018-01-22 18:14:16 +0545

    error fixing latest blog

--- front-page.php
+++ front-page.php
@@ -1,16 +1,27 @@
 <?php
 /**
+ * Template Name: Front page
  * The template for displaying home page.
- * @package business-craft
+ * @package Flare Pro
  */
 global $business_craft_customizer_all_values;
-
 get_header();
-if ( 'posts' == get_option( 'show_on_front' ) )
-{
+if ( 'posts' == get_option( 'show_on_front' ) ) {
     include( get_home_template() );
-}
-if ( ( $business_craft_customizer_all_values['business-craft-feature-slider-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-home-feature-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-about-us-enable-option'] != 1 ) && ($business_craft_customizer_all_values['business-craft-our-service-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-single-button-enable'] != 1 ) && ( $business_craft_customizer_all_values['business-craft-home-testimonial-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-home-blog-enable'] != 1 )) {
+    }
+else{   
+    /**
+     * flare_homepage hook
+     * @since Charitize 1.0.0
+     *
+     * @hooked flare_homepage -  10
+     * @sub_hooked flare_homepage -  30
+     */
+    do_action( 'business_craft_homepage' );
+
+    $business_craft_static_page = absint($business_craft_customizer_all_values['business-craft-enable-static-page']);
+    
+    if ( ( $business_craft_customizer_all_values['business-craft-feature-slider-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-home-feature-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-about-us-enable-option'] != 1 ) && ($business_craft_customizer_all_values['business-craft-our-service-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-single-button-enable'] != 1 ) && ( $business_craft_customizer_all_values['business-craft-home-testimonial-enable'] != 1 ) && ($business_craft_customizer_all_values['business-craft-home-blog-enable'] != 1 )) {
         if ( current_user_can( 'edit_theme_options' ) ) { ?>
             <section class="wrapper display-info">
                 <div class="container">
@@ -21,23 +32,9 @@ if ( ( $business_craft_customizer_all_values['business-craft-feature-slider-enab
                 </div>
             </section>
         <?php }
-    }   
-    else
-    {
-		/**
-		 * business_craft_homepage hook
-		 * @since business-craft 0.0.1
-		 *
-		 * @hooked business_craft_homepage -  10
-		 * @sub_hooked business_craft_homepage -  30
-         * @hooked busine_Craft_aboutus _page -16
-         * @hooked business_craft_our_service -21
-		 */
-        do_action( 'business_craft_homepage' );
-
-        $business_craft_static_page = absint($business_craft_customizer_all_values['business-craft-enable-static-page']);
-        
-        // $business_craft_static_page = 1;
+        else
+        {
+            // $business_craft_static_page = 1;
         if ($business_craft_static_page == 1) { ?>
             <div id="content" class="site-content container">
                 <div id="primary" class="content-area col-sm-8">
@@ -64,5 +61,9 @@ if ( ( $business_craft_customizer_all_values['business-craft-feature-slider-enab
                 
             </div>
         <?php }
+        }
+    }   
+   
+        
     }
 get_footer();
