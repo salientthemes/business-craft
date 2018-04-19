3f75131623844b13be68e7bcc15c02cb35fbfd12
kabiraj lamichhane <cloudkabiraj@gmial.com>
2018-04-19 18:27:29 +0545

    remove color options

--- inc/customizer/color/color-section.php
+++ inc/customizer/color/color-section.php
@@ -7,7 +7,7 @@ global $business_craft_customizer_defaults;
 $business_craft_customizer_defaults['business-craft-site-identity-color'] = '#313131';
 $business_craft_customizer_defaults['business-craft-primary-color'] = '#0288d1';
 $business_craft_customizer_defaults['business-craft-heading-title-color'] = '#000000';
-$business_craft_customizer_defaults['business-craft-color-reset'] = '';
+// $business_craft_customizer_defaults['business-craft-color-reset'] = '';
 
 /*Default color*/
 $business_craft_sections['colors'] = array(
@@ -22,32 +22,33 @@ $business_craft_sections['colors'] = array(
  *
  * @since business-craft 1.0.0
  */
-if ( ! function_exists( 'salient_news_color_reset' ) ) :
-    function salient_news_color_reset( ) {
+// if ( ! function_exists( 'business_craft_color_reset' ) ) :
+//     function business_craft_color_reset( ) {
         
-        global$business_craft_customizer_saved_values;
-        $business_craft_customizer_saved_values = business_craft_get_all_options();
-        if ($business_craft_customizer_saved_values['business-craft-color-reset'] == 1 ) {
-            global$business_craft_customizer_defaults;
-            global$business_craft_customizer_saved_values;
-            /*getting fields*/
+//         global$business_craft_customizer_saved_values;
+//         $business_craft_customizer_saved_values = business_craft_get_all_options();
+//         if ($business_craft_customizer_saved_values['business-craft-color-reset'] == 1 ) {
+//             global$business_craft_customizer_defaults;
+//             global$business_craft_customizer_saved_values;
+//             /*getting fields*/
 
-            /*setting fields */
-           $business_craft_customizer_saved_values['business-craft-site-identity-color'] =$business_craft_customizer_defaults['business-craft-site-identity-color'] ;
-           $business_craft_customizer_saved_values['business-craft-primary-color'] =$business_craft_customizer_defaults['business-craft-primary-color'] ;
-           $business_craft_customizer_saved_values['business-craft-heading-title-color'] = $business_craft_customizer_defaults['business-craft-heading-title-color'];
+//             /*setting fields */
+//            $business_craft_customizer_saved_values['business-craft-site-identity-color'] =$business_craft_customizer_defaults['business-craft-site-identity-color'] ;
+//            $business_craft_customizer_saved_values['business-craft-primary-color'] =$business_craft_customizer_defaults['business-craft-primary-color'] ;
+//            $business_craft_customizer_saved_values['business-craft-heading-title-color'] = $business_craft_customizer_defaults['business-craft-heading-title-color'];
           
-            remove_theme_mod( 'background_color' );
-           $business_craft_customizer_saved_values['business-craft-color-reset'] = '';
-            /*resetting fields*/
-            salient_news_reset_options($business_craft_customizer_saved_values );
-        }
-        else {
-            return '';
-        }
-    }
-endif;
-add_action( 'customize_save_after','salient_news_color_reset' );
+//             remove_theme_mod( 'background_color' );
+//            $business_craft_customizer_saved_values['business-craft-color-reset'] = '';
+//             /*resetting fields*/
+//             business_craft_reset_options($business_craft_customizer_saved_values );
+            
+//         }
+//         else {
+//             return '';
+//         }
+//     }
+// endif;
+// add_action( 'customize_save_after','business_craft_color_reset' );
 
 
 
@@ -97,17 +98,17 @@ $business_craft_settings_controls['business-craft-heading-title-color'] = array(
 
 
 
-$business_craft_settings_controls['business-craft-color-reset'] = array(
-    'setting' => array(
-        'default'   => $business_craft_customizer_defaults['business-craft-color-reset'],
-        'transport'            => 'postmessage',
-    ),
-    'control' => array(
-        'label'                 =>  esc_html__( 'Reset', 'business-craft' ),
-        'description'           =>  esc_html__( 'Caution: Reset all color settings above to default. Refresh the page after saving to view the effects', 'business-craft' ),
-        'section'               => 'colors',
-        'type'                  => 'checkbox',
-        'priority'              => 220,
-        'active_callback'       => ''
-    )
-);
+// $business_craft_settings_controls['business-craft-color-reset'] = array(
+//     'setting' => array(
+//         'default'   => $business_craft_customizer_defaults['business-craft-color-reset'],
+//         'transport'            => 'postmessage',
+//     ),
+//     'control' => array(
+//         'label'                 =>  esc_html__( 'Reset', 'business-craft' ),
+//         'description'           =>  esc_html__( 'Caution: Reset all color settings above to default. Refresh the page after saving to view the effects', 'business-craft' ),
+//         'section'               => 'colors',
+//         'type'                  => 'checkbox',
+//         'priority'              => 220,
+//         'active_callback'       => ''
+//     )
+// );
